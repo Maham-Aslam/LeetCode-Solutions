ce5ae91a13adfa4c043b60f167362b8f590543d8
Maham Aslam Alvi <108095594+Maham-Aslam@users.noreply.github.com>
2022-08-03 17:44:58 +0500

    Time: 22 ms (6.10%), Space: 8.2 MB (5.31%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        int count = 0;
+        unordered_map<char, string> umap;
+        
+        int len = s.length();
+        for(int i=0; i<len; i++) {
+            if(umap.find(s[i]) == umap.end())
+                umap[s[i]] = "";
+            else {
+                umap.erase(s[i]);
+                count += 2;
+            }
+        }
+        
+        if(count == len)
+            return count;
+        return count+1;
+    }
+};
