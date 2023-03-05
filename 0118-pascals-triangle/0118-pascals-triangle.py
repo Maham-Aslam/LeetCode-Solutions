15499f1ea6d4118c34dfa135afd03ce2105113de
Maham Aslam Alvi <108095594+Maham-Aslam@users.noreply.github.com>
2023-03-05 16:41:44 +0500

    Time: 37 ms (52.57%), Space: 14 MB (14.05%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -0,0 +1,18 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = [[1],[1,1]]
+        
+        for i in range(2,numRows):
+            temp = []
+            #   1st element will always be 1
+            temp.append(1)
+            #   compute sum
+            size = len(res[i-1])
+            for j in range(size-1):
+                temp.append(res[i-1][j]+res[i-1][j+1])
+            #   last element will always be 1
+            temp.append(1)
+            res.append(temp)
+            
+        return res[:numRows]
+            
