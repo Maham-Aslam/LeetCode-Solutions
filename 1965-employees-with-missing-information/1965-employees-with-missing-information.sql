5bb7b3c8b81a5b86436ba938a96aa4d943b96dad
Maham Aslam Alvi <108095594+Maham-Aslam@users.noreply.github.com>
2023-03-07 02:16:19 +0500

    Time: 574 ms (72.21%), Space: 0B (100.00%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -4,13 +4,10 @@ FROM Employees
 LEFT JOIN Salaries 
 ON Employees.employee_id = Salaries.employee_id
 WHERE salary IS NULL
-
 UNION ALL
-
 SELECT Salaries.employee_id
 FROM Employees
 RIGHT JOIN Salaries
 ON Employees.employee_id = Salaries.employee_id
 WHERE name IS NULL
-
 ORDER BY employee_id
